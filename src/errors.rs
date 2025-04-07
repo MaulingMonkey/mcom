dbb01e8581a09eb733a0c72677e8846cb9f15c42
MaulingMonkey <git@maulingmonkey.com>
2025-04-07 14:57:16 -0700

    MethodHResult::hresult_info_search_link: deprecate

--- src/errors.rs
+++ src/errors.rs
@@ -27,6 +27,7 @@ impl MethodHResult {
     pub fn hresult(&self) -> HRESULT { self.hr }
 
     /// Returns a link in the format of e.g. "<https://www.hresult.info/Search?q=0x80000005>"
+    #[deprecated = "This function will be removed in 0.2.0"]
     pub fn hresult_info_search_link(&self) -> alloc::string::String { alloc::format!("https://www.hresult.info/Search?q=0x{:08x}", self.to_u32()) }
 }
 
